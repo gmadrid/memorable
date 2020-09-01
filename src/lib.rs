32abbbd1861c63279edfb5bb9b93e312f9e1dca6
George Madrid <gmadrid@gmail.com>
2020-09-01 03:19:20 -0400

    Adding docs.

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,14 @@ use rand::{thread_rng, Rng};
 mod adjectives;
 mod nouns;
 
+/// Returns a two-word memorable id.
 pub fn memorable_id_simple() -> String {
     let mut rng = thread_rng();
     memorable_id(&mut rng)
 }
 
+/// Returns a two-word memorable id using the supplied rng as a source
+/// of randomness.
 pub fn memorable_id(mut rng: &mut impl Rng) -> String {
     let adj = rand_word(&mut rng, adjectives::ADJECTIVES);
     let noun = rand_word(&mut rng, nouns::NOUNS);
